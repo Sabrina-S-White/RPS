c7b0d9753ce881700b82799ee874aa25ee457729
sabrinaMac <sabrina.scherma@gmail.com>
2022-08-16 13:11:12 -0600

    Finished game functionality and interaction with html.

--- rps2.js
+++ rps2.js
@@ -1,13 +1,18 @@
 let compScore = 0;
 let playScore = 0;
+const buttons = document.querySelectorAll('.btn'); 
 
 document.getElementById('para').innerHTML = 'Welcome! To play a best of five round of Rock, Paper, Scissors click one of the buttons below!';
-
-const buttons = document.querySelectorAll('.btn'); 
 
 function getComputerChoice () {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
+};
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    });
 };
     
 function playRound(selection) {
@@ -18,6 +23,10 @@ function playRound(selection) {
         (selection == 'scissors') && (compChoice == 'paper')) {
         ++playScore
         document.getElementById('result').innerHTML = ("Congratulations, you won the round! The score is: <br>" + "Player Score: " + `${playScore}` + "<br>" + "Computer Score: " + `${compScore}`);
+        if (playScore == 5) {
+            document.getElementById('result').innerHTML = ('Congratulations, you won the game! ' + "The final score is:<br>" + 'Player Score: ' + `${playScore}` + "<br>" + "Computer Score: " + `${compScore}` + "<br>" + "Refresh to play again!");
+            disableButtons();
+        };
     } else if (selection == compChoice) {
         document.getElementById('result').innerHTML = ("It's a tie! You both chose " + `${selection}` + ". The score is: <br>" + "Player Score: " + `${playScore}` + "<br>" + "Computer Score: " + `${compScore}`)
     } else if ((selection == 'rock') && (compChoice == 'paper') ||
@@ -25,17 +34,15 @@ function playRound(selection) {
         (selection == 'scissors') && (compChoice == 'rock')) {
         ++compScore;
         document.getElementById('result').innerHTML = ("Sorry, you lost the round. The score is: <br>" + "Player Score: " + `${playScore}` + "<br>" + "Computer Score: " + `${compScore}`);
+        if (compScore == 5) {
+            document.getElementById('result').innerHTML = ("Sorry, you lost. The final score is:<br>" + "Player Score: " + `${playScore}` + "<br>" + "Computer Score: " + `${compScore}` + "<br>" + "Refresh to play again!");
+            disableButtons();
+        };
     };
 };
 
-function print(hmm) {
-    document.getElementById('result').innerHTML = "You clicked " + `${hmm}`;
-}
-
-
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        console.log('you clicked ' + `${button.value}`);
         playRound(button.value);
     })
 });
